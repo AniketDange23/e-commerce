5555b27a0c892fe17ec94fec8581963903973b7a
AniketDange23 <Aniketdange2321@gmail.com>
2023-01-05 02:39:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const videos = document.querySelectorAll('video');
+
+videos.forEach(video => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+  video.addEventListener('mouseout', () => {
+    video.pause();
+  });
+});
